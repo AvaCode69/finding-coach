127c9f0e03d0389c86742a27889d0fb2f90f1b58
AvaCode69 <reacthmd34@gmail.com>
2023-12-28 13:28:41 +0100

    set up Coaches List and List Items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,9 +6,7 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coache in filterCoaches" :key="coache.id">
-        {{ coache.firstName }}
-      </li>
+      <coach-item v-for="coach in filterCoaches" :key="coach.id" :coachInfo="coach"></coach-item>
     </ul>
     <h3 v-else>No coaches found</h3>
   </section>
@@ -16,7 +14,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import CoachItem from '../../components/coaches/CoachItem.vue'
 export default {
+  components: {
+    CoachItem
+  },
   computed: {
     filterCoaches() {
       return this.$store.getters['coaches/coaches']
@@ -28,3 +30,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
