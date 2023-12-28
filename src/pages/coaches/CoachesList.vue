a5c42e891873879efce8593bf2ca3c6d83cf0d20
AvaCode69 <reacthmd34@gmail.com>
2023-12-28 17:02:17 +0100

    Adding a Couple of Base Components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,23 +1,30 @@
 <template>
   <section>Filter</section>
-  <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item v-for="coach in filterCoaches" :key="coach.id" :coachInfo="coach"></coach-item>
-    </ul>
-    <h3 v-else>No coaches found</h3>
-  </section>
+  <base-card>
+    <section>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filterCoaches" :key="coach.id" :coachInfo="coach"></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </section>
+  </base-card>
 </template>
 
 <script>
+import BaseCard from '@/components/ui/BaseCard.vue'
 import { mapGetters } from 'vuex'
 import CoachItem from '../../components/coaches/CoachItem.vue'
+import BaseButton from '@/components/ui/BaseButton.vue'
+
 export default {
   components: {
-    CoachItem
+    CoachItem,
+    BaseCard,
+    BaseButton
   },
   computed: {
     filterCoaches() {
