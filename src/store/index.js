ce0d59070ded498cf6dfa0719cdee7976e394938
AvaCode69 <reacthmd34@gmail.com>
2024-01-01 22:04:23 +0100

    set up contact form

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
 
 import coachModules from './modules/coaches/index'
-import requestsModules from './modules/coaches/index'
+import requestsModules from './modules/requests/index'
 
 const store = createStore({
   modules: {
