3f3eb6c2a91daa8118cb4525ae343fcf544393ce
AvaCode69 <reacthmd34@gmail.com>
2024-01-03 17:53:57 +0100

    set firebase & Adding a "Signup" Action & Flow

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,13 @@
 import { createStore } from 'vuex'
-
-import coachModules from './modules/coaches/index'
-import requestsModules from './modules/requests/index'
+import authModule from './modules/auth/index'
+import coachModule from './modules/coaches/index'
+import requestsModule from './modules/requests/index'
 
 const store = createStore({
   modules: {
-    coaches: coachModules,
-    requests: requestsModules
-  },
-  state() {
-    return {
-      userId: 'c3'
-    }
-  },
-  getters: {
-    userId(state) {
-      return state.userId
-    }
+    coaches: coachModule,
+    requests: requestsModule,
+    auth: authModule
   }
 })
 
