9f0d3e3e45f97a36f65904170c43acecb3ecfbe7
AvaCode69 <reacthmd34@gmail.com>
2024-01-05 01:11:03 +0100

    Add localStorage  Auth Login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,16 +1,33 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyClCN1aAbxoEnPHexXOauRx1sAyMW9k4l4',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    )
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    })
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    })
+  },
+  async auth(context, payload) {
+    const mode = payload.mode
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyClCN1aAbxoEnPHexXOauRx1sAyMW9k4l4'
+
+    if (mode == 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyClCN1aAbxoEnPHexXOauRx1sAyMW9k4l4'
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    })
 
     const responseData = await response.json()
 
@@ -21,6 +38,8 @@ export default {
       )
       throw error
     }
+    localStorage.setItem('token', responseData.idToken)
+    localStorage.setItem('userId', responseData.localId)
 
     console.log(responseData)
     context.commit('setUser', {
@@ -29,32 +48,16 @@ export default {
       tokenExpiration: responseData.expiresIn
     })
   },
-  async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyClCN1aAbxoEnPHexXOauRx1sAyMW9k4l4',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    )
-
-    const responseData = await response.json()
-
-    if (!response.ok) {
-      console.log(responseData)
-      const error = new Error(responseData.message || 'Failed to authenticate.')
-      throw error
+  rtyLogin(context) {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      })
     }
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
-    })
   },
   logout(context) {
     context.commit('setUser', {
